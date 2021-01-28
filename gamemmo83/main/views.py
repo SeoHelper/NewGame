e54c6c1f5e59478cd9e0c4bb241ce68115f02114
SeoHelper <78032427+SeoHelper@users.noreply.github.com>
2021-01-29 00:41:50 +0500

    Delete gamemmo83 directory

--- gamemmo83/main/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from games.models import Gamelist
-
-def index(request):
-    games = Gamelist.objects.all()[1:3]
-    return render(request, 'main/index.html', {'games':games})
